f83289af03e53dcf1034d74b6b5c69a75edfc5d7
Meqativ <meqativ@gmail.com>
2023-04-23 00:23:52 +0300

    ok

--- tags/repeat.js
+++ tags/repeat.js
@@ -14,7 +14,7 @@ const defaultConfig = {
 	},
 };
 
-const repeatJob = (config) => {
+const repeatJob = (cfg) => {
 	const BASE_REPO_URL = "https://github.com/Meqativ/ass-yst-tags/blob/main",
 		YOU = "cute"
 	if (config?.assyst?.args === "raw") return BASE_REPO_URL+"/repeat.js"
