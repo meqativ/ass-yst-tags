b4949730edc61613e03aed9e1bb008ac569c9c67
Meqativ <meqativ@gmail.com>
2023-06-16 23:46:56 +0300

    h

---
+++ tags/connections.js
@@ -0,0 +1,59 @@
+/* Add this as a tag on your server
+-t create connections {js:
+{download:https://github.com/Meqativ/ass-yst-tags/raw/main/tags/connections.js?update={js:+Date.now()}}
+connectionsJob(\{...defaultConfig\});
+}
+*/
+const getAvailableConnections = () =>
+	fetch("https://canary.discord.com/api/v9/connections/meoww/authorize")
+		.then((res) => res.json())
+		.then((json) => json.errors.provider_id._errors[0].message.match(/(?<=')[\w-]+(?=')/g).sort());
+
+const BASE_REPO_URL = "https://github.com/Meqativ/ass-yst-tags/blob/main",
+	YOU = "cute",
+	TAG_PREFIX = message.content.replace(args.join(""), "");
+const defaultConfig = {
+	version: 0, // number, 0
+	assyst: {
+		args: undefined, // undefined (uses the constant) | string // {replace:"|\"|{args}}
+	},
+};
+const emojis = {
+	bad_image: "<:bad_image:1105128972364283954>",
+	help: "<:questionmark:1099298038125695068>",
+	help2: "<a:dumbass_think_already:1099938781504745483>",
+};
+
+const connectionsJob = async (cfg) => {
+	if (cfg === undefined) cfg = {};
+	let args = args ?? undefined;
+	if (typeof cfg?.assyst?.args === "string") args = cfg.assyst.args.split(" ");
+	if (typeof args !== "string") throw new Error("No args");
+	if (config?.assyst?.args === "raw") return BASE_REPO_URL + "/tags/connections.js";
+	args[0] = args[0].toLowerCase();
+
+	if (args[0] === "services") return getAvailableConnections().then((arr) => `The available services are:\n1. \`${arr.join("\n1.")}\`.`);
+
+	if (args[0] === "snippet") {
+		const connections = await getAvailableConnections();
+		let service;
+		let isValid;
+		if (args[1]) isValid = connections.includes(args[1]);
+		if (isValid) service = args[1];
+		return (
+			`\`\`\`js\nconst service = "${service || "SERVICE_HERE"}";${
+				isValid === true ? "" : `  // ${isValid === false ? `${args[1]} isn't a valid service, do ` : ""}\"${TAG_PREFIX} connections services\" for a list of available services`
+			}\n` + "(await (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m => m?.exports?.Z?.authorize).exports.Z.authorize(service)).body.url```"
+		);
+	}
+	return (
+		`0. Open https://discord.com/app in a browser(preferably chrome), and go into the devtools console by pressing \`Ctrl + Option + J\` ||[Option is the windows key, or ⌘ on mac]||\n` +
+		`1. Do "${TAG_PREFIX} snippet" and copy the snippet into the console text input\n` +
+		`2. Fill in the blanks ("SERVICE_HERE"), or add the wanted connection as the argument to the snippet subcommand\n` +
+		`3. Press enter\n` +
+		`4. Go to the link and follow the steps on there\n` +
+		`5. 🎉`
+	);
+};
+
+const run = (rawArgs) => connectionsJob({ assyst: { args: rawArgs } });
--- tags/sonicsays.js
+++ tags/sonicsays.js
@@ -10,43 +10,32 @@ const defaultConfig = {
 	},
 };
 const emojis = {
-	bad_image: "<:bad_image:1105128972364283954>",
-	help: "<:questionmark:1099298038125695068>",
-	help2: "<a:dumbass_think_already:1099938781504745483>",
-};
+		bad_image: "<:bad_image:1105128972364283954>",
+		help: "<:questionmark:1099298038125695068>",
+		help2: "<a:dumbass_think_already:1099938781504745483>",
+	},
+	BASE_REPO_URL = "https://github.com/Meqativ/ass-yst-tags/blob/main",
+	YOU = "cute";
 
 const sonicSaysJob = async (cfg) => {
 	if (!cfg) cfg = {};
 	if (!cfg?.text) cfg.text = cfg?.assyst.args;
-	if (cfg?.version !== 0)
-		throw new Error(
-			`Version ${cfg.version} isn't available, use one of these: 0`
-		);
-	if (cfg?.text === "pastebin") return `https://pastebin.com/FW0h599S`;
-	if (typeof cfg?.fontSize !== "number" && typeof cfg?.fontSize !== "undefined")
-		throw new Error(`Font size should be a number`);
+	if (cfg?.version !== 0) throw new Error(`Version ${cfg.version} isn't available, use one of these: 0`);
+	if (config?.assyst?.args === "raw") return BASE_REPO_URL + "/tags/sonicsays.js";
+	if (typeof cfg?.fontSize !== "number" && typeof cfg?.fontSize !== "undefined") throw new Error(`Font size should be a number`);
 	if (cfg?.assyst?.args.length === 0) {
 		ctx.usesSONICSAYS ??= {};
-		if (!ctx.usesSONICSAYS[message.author.id])
-			ctx.usesSONICSAYS[message.author.id] = 0;
+		if (!ctx.usesSONICSAYS[message.author.id]) ctx.usesSONICSAYS[message.author.id] = 0;
 		ctx.usesSONICSAYS[message.author.id] += 1;
-		return `${
-			ctx.usesSONICSAYS[message.author.id] < 5 ? emojis.help : emojis.help2
-		}\`-t sonicsays [text...]\``;
-		if (message?.referenced_message && message.referenced_message.content)
-			cfg.text = message.referenced_message.content;
+		return `${ctx.usesSONICSAYS[message.author.id] < 5 ? emojis.help : emojis.help2}\`-t sonicsays [text...]\``;
+		if (message?.referenced_message && message.referenced_message.content) cfg.text = message.referenced_message.content;
 	}
 	try {
 		const [sonic, font] = await Promise.all([
-			fetch(
-				`https://cdn.discordapp.com/attachments/824020889234440232/1083513327700951093/Untitled160_20230310001307.png`
-			)
+			fetch(`https://cdn.discordapp.com/attachments/824020889234440232/1083513327700951093/Untitled160_20230310001307.png`)
 				.then((res) => res.arrayBuffer())
 				.then(ImageScript.decode),
-			fetch(
-				cfg?.font ??
-					`https://cdn.discordapp.com/attachments/824020889234440232/1084046492865662996/font.ttf`
-			)
+			fetch(cfg?.font ?? `https://cdn.discordapp.com/attachments/824020889234440232/1084046492865662996/font.ttf`)
 				.then((res) => res.arrayBuffer())
 				.then((a) => new Uint8Array(a)),
 		]);
@@ -59,17 +48,12 @@ const sonicSaysJob = async (cfg) => {
 			horizontalAlign: "top",
 		});
 
-		let textOverlay = await ImageScript.Image.renderText(
-			font,
-			cfg?.fontSize -
-				parseInt(cfg?.text.length) / (parseInt(cfg?.text.length) / 5),
-			cfg?.text,
-			0xffffffee,
-			layout
-		);
+		let textOverlay = await ImageScript.Image.renderText(font, cfg?.fontSize - parseInt(cfg?.text.length) / (parseInt(cfg?.text.length) / 5), cfg?.text, 0xffffffee, layout);
 		sonic.composite(textOverlay, 52, 119, 0);
 		return sonic.encode(cfg?.encodeLevel);
 	} catch (error) {
-		return `${emojis.bad_image} Failed to generate image${(error.message.includes("RangeError: Image has to be at least 1 pixel wide")) ? ". (This tag doesn't support emojis and some other special characters)" : ""}\`\`\`js\n${error.stack}\`\`\``;
+		return `${emojis.bad_image} Failed to generate image${
+			error.message.includes("RangeError: Image has to be at least 1 pixel wide") ? ". (This tag doesn't support emojis and some other special characters)" : ""
+		}\`\`\`js\n${error.stack}\`\`\``;
 	}
 };
