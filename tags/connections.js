239fa00ce165d35288b41e1a71b8f678f4f8bf21
Meqativ <meqativ@gmail.com>
2023-06-17 00:10:05 +0300

    h

--- tags/connections.js
+++ tags/connections.js
@@ -26,7 +26,7 @@ const emojis = {
 const _args = [...args];
 const connectionsJob = async (cfg) => {
 	if (cfg === undefined) cfg = {};
-	const args = _args.length !== 0 ? _args : cfg?.assyst?.args.?split?.(" ");
+	const args = _args.length !== 0 ? _args : cfg?.assyst?.args?.split?.(" ");
 	if (args.length === undefined) throw new Error("No args")
 	args[0] = args[0].toLowerCase();
 	if (args[0] === "raw") return BASE_REPO_URL + "/tags/connections.js";
